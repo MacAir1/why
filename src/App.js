f40ad646f2d539994e2f4c767c6df88f586bdf26
MacAir1 <swift4925@gmail.com>
2023-04-26 16:45:34 +0900

    last commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import TicTacToe from './TicTacToe/TicTacToe';
+import "./App.css";
+import TicTacToe from "./TicTacToe/TicTacToe";
 
 function App() {
-	return (
-		<div className='App'>
-			<TicTacToe />
-		</div>
-	);
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
 }
 
 export default App;
